a80120f4f4e429fb6591a2e36b64db732dc4feee
MeriAv87 <112964377+MeriAv87@users.noreply.github.com>
2022-12-10 20:29:36 +0400

    Update Test2.java

--- src/pac1/Test2.java
+++ src/pac1/Test2.java
@@ -3,5 +3,7 @@ package pac1;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println(" First gitHub practice ");
+        
+        //I finished my code
     }
 }
